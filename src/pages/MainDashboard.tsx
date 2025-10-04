8defdbc1bfb413afc715b1d29d9a7c83182834fa
Nicolas Dyhrman <nicolas.dyhrman@gmail.com>
2025-10-05 00:33:12 +0200

    Fix update system: add process relaunch, use actions instead of btn, fix Modal import

--- src/pages/MainDashboard.tsx
+++ src/pages/MainDashboard.tsx
@@ -3,7 +3,7 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Col, Statistic, Space, Tabs, Avatar, TabsProps, App, InputNumber } from 'antd';
+import { Card, Row, Col, Statistic, Space, Tabs, Avatar, TabsProps, App, InputNumber, Modal } from 'antd';
 import {
   TeamOutlined,
   HeartOutlined,
